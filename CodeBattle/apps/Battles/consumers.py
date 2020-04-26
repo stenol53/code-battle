90bf8154aea3a52a8c23af463a808a3daa9c3e3f
firmfreez <aleksanyanc@list.ru>
2020-04-26 15:58:24 +0300

    wegwgw

--- CodeBattle/apps/Battles/consumers.py
+++ CodeBattle/apps/Battles/consumers.py
@@ -81,7 +81,7 @@ class BattleConsumer(AsyncConsumer):
                                         'start_session': True,
                                         'other_user_name': opp.name,
                                         'other_sername' : opp.sirname,
-                                        'other_login' : opp,
+                                        'other_login' : opp.username,
                                         'questions_count': self.battle.count,
                                     }
                                     good = True
@@ -101,7 +101,7 @@ class BattleConsumer(AsyncConsumer):
                             good = True
             if json_response["type"] == "session":
                 print(json_response)
-
+                opp = await self.get_opponent()
                 if json_response["id"] == self.me.id:
                     Response = {
                         'type': 'session',
@@ -109,7 +109,7 @@ class BattleConsumer(AsyncConsumer):
                         'start_session': True,
                         'other_user_name': self.me.name,
                         'other_sername' : self.me.sirname,
-                        'other_login' : self.me,
+                        'other_login' : self.me.username,
                         'questions_count': self.battle.count,
                     }
                     good = True
